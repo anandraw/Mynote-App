d9c1b23e216bb09ae6eb84569c885320f985498e
Anand Rawool <anandrawool5624@gmail.com>
2025-05-31 23:13:58 +0530

    first commit

---
+++ src/main/java/org/anand/mynoteapp/MynoteAppApplication.java
@@ -0,0 +1,13 @@
+package org.anand.mynoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynoteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynoteAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/anand/mynoteapp/MynoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anand.mynoteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynoteAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
