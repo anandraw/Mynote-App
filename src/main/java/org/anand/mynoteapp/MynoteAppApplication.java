66e14957a26693604e49108fa97d156f6cfd6665
Anand Rawool <anandrawool5624@gmail.com>
2025-06-03 12:04:16 +0530

    auditing-category
    aduding done in category module

--- src/main/java/org/anand/mynoteapp/MynoteAppApplication.java
+++ src/main/java/org/anand/mynoteapp/MynoteAppApplication.java
@@ -2,8 +2,10 @@ package org.anand.mynoteapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MynoteAppApplication {
 
     public static void main(String[] args) {
