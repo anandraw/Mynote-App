77cac9e4b39e2088d1bf7ca2cacea10edf3b81a5
Anand Rawool <anandrawool5624@gmail.com>
2025-06-15 13:07:17 +0530

    update-emailvalidation

--- src/main/java/org/anand/mynoteapp/repository/UserRepository.java
+++ src/main/java/org/anand/mynoteapp/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.anand.mynoteapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Boolean existsByEmail(String email);
 }
